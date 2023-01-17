f850172c77dd95d79842b5d14a974ac6f450f955
Yaourt <soj.data@gmail.com>
2023-01-17 09:27:33 +0100

    Conversations en cours

--- cogs/active_threads.py
+++ cogs/active_threads.py
@@ -8,10 +8,6 @@ from tools.text_importers import read_yaml
 
 class ActiveThreads(commands.Cog):
 
-    """
-    Builds and publishes list of currently active threads on the server
-    """
-
     def __init__(self, bot):
         self.__bot = bot
         self.__guild = self.__bot.guilds[0]
@@ -27,24 +23,14 @@ class ActiveThreads(commands.Cog):
         return read_yaml('config/active_threads.yml')
 
     def __initialize_run(self) -> None:
-
-        """
-        Resets settings and date.
-        """
-
         self.__date = datetime.now(tz=timezone.utc)
         self.__settings = self.__read_settings()
 
     @tasks.loop(hours=1)
     async def refresh_open_threads_list(self) -> None:
-
-        """
-        Extracts open threads list, builds messages and posts them.
-        """
-
         logger = Logger(
             self.__bot,
-            log_group='Tâche automatisée',
+            log_group='Tâche automatisée ',
             message_start=f'Mise à jour du salon {self.__channel_to_update.mention}.',
             message_success=f'Mise à jour du salon {self.__channel_to_update.mention} terminée.',
             task_info='task.update.active threads'
@@ -54,19 +40,13 @@ class ActiveThreads(commands.Cog):
 
         self.__initialize_run()
         await self.__build_channel_thread_list()
-        await self.__build_messages()
-        await self.__clean_channel()
-        await self.__update_channel()
+        await self.__build_message_list()
+        await self.__update_active_threads_channel()
 
         await logger.log_success()
         return
 
     async def __build_channel_thread_list(self) -> None:
-
-        """
-        Clears and re-populates the __channel_threads property.
-        """
-
         self.__channel_threads = []
         threads = await self.__guild.active_threads()
 
@@ -78,47 +58,42 @@ class ActiveThreads(commands.Cog):
                 "channel": channel,
                 "threads": [thread for thread in threads_channel if not await self.__thread_is_dead(thread)]
             }
+
             if len(channel_dictionary["threads"]) > 0:
                 self.__channel_threads.append(channel_dictionary)
 
             for thread in threads_channel:
                 threads.remove(thread)
-        return
 
-    async def __thread_is_dead(self, thread):
-        archive_date = await self.__get_archive_date(thread)
+    async def __thread_is_dead(self, thread) -> bool:
+        archive_date = await self.__get_thread_archive_date(thread)
         return archive_date < datetime.now(tz=timezone.utc)
 
-    async def __get_archive_date(self, thread) -> datetime:
+    async def __get_thread_archive_date(self, thread) -> datetime:
         up_timestamp = thread.archive_timestamp
         try:
-            last_message = await self.__get_last_message_date(thread)
+            last_message = await self.__get_thread_last_message_date(thread)
         except:
             last_message = up_timestamp
         last_activity = last_message if last_message > up_timestamp else up_timestamp
         return last_activity + timedelta(minutes=thread.auto_archive_duration)
 
     @staticmethod
-    async def __get_last_message_date(thread) -> datetime:
+    async def __get_thread_last_message_date(thread) -> datetime:
         message_list = []
         async for item in thread.history(limit=1):
             message_list.append(item)
         last_message = message_list[0]
         return last_message.created_at
 
-    async def __build_messages(self) -> None:
-
-        """
-        Builds a list of messages to post in the Open Threads channel
-        """
-
+    async def __build_message_list(self) -> None:
         instructions = f"""
-        Au {self.__date.astimezone(tz=tz.gettz('Europe/Paris')).strftime("%d/%m/%Y, %H:%M:%S")}, voici la liste des fils actifs sur ce serveur Discord.
-        
-        **__Légende :__**
-        > :{self.__settings["new"]["emoji"]}: = {self.__settings["new"]["description"]}
-        > :{self.__settings["up"]["emoji"]}: = {self.__settings["up"]["description"]}
-        > :{self.__settings["dying"]["emoji"]}: = {self.__settings["dying"]["description"]}
+Au {self.__date.astimezone(tz=tz.gettz('Europe/Paris')).strftime("%d/%m/%Y, %H:%M:%S")} (heure de Paris), voici la liste des fils actifs sur ce serveur Discord.
+
+**__Légende :__**
+> :{self.__settings["new"]["emoji"]}: = {self.__settings["new"]["description"]}
+> :{self.__settings["up"]["emoji"]}: = {self.__settings["up"]["description"]}
+> :{self.__settings["dying"]["emoji"]}: = {self.__settings["dying"]["description"]}
         """
 
         self.__messages = [instructions]
@@ -128,77 +103,56 @@ class ActiveThreads(commands.Cog):
 
             for thread in channel["threads"]:
                 thread_string = f"\n> {thread.mention}"
-                thread_string += self.__check_if_new(thread)
-                thread_string += self.__check_if_up(thread)
-                thread_string += await self.__check_if_dying(thread)
+                thread_string += self.__add_new_emoji_if_required(thread)
+                thread_string += self.__add_up_emoji_if_required(thread)
+                thread_string += await self.__add_dying_emoji_if_required(thread)
                 self.__complete_last_message(thread_string)
 
         return
 
     def __complete_last_message(self, string_to_add) -> None:
+        last_message_length = len(self.__messages[-1])
+        string_to_add_length = len(string_to_add)
 
-        """
-        Adds the string to the last item of the __messages property if it doesn't exceed the character limit.
-        Otherwise, appends this string to the list.
-
-        :param string_to_add: The string to add to the last message
-        """
-
-        message_length = len(self.__messages[-1])
-        string_length = len(string_to_add)
-
-        if message_length + string_length <= self.__settings["message_length"]:
+        if last_message_length + string_to_add_length <= self.__settings["max_message_length"]:
             self.__messages[-1] += string_to_add
         else:
             self.__messages.append(string_to_add)
 
-        return
-
-
-    def __check_if_new(self, thread) -> str:
+    def __add_new_emoji_if_required(self, thread) -> str:
         parameters = self.__settings["new"]
         hours_since_creation = (self.__date - thread.created_at).total_seconds() / 3600
         hours_for_emoji = parameters["hours"]
-        output = f" :{parameters['emoji']}:" if hours_since_creation < hours_for_emoji else ''
+        output = f" :{parameters['emoji']}:" if hours_since_creation < hours_for_emoji else ""
         return output
 
-    def __check_if_up(self, thread) -> str:
+    def __add_up_emoji_if_required(self, thread) -> str:
         parameters = self.__settings["up"]
         thread_is_new = thread.archive_timestamp + timedelta(minutes=-5) < thread.created_at
         hours_since_up = (self.__date - thread.archive_timestamp).total_seconds() / 3600
         hours_for_emoji = parameters["hours"]
-        output = f" :{parameters['emoji']}:" if hours_since_up < hours_for_emoji and not thread_is_new else ''
+        output = f" :{parameters['emoji']}:" if hours_since_up < hours_for_emoji and not thread_is_new else ""
         return output
 
-    async def __check_if_dying(self, thread) -> str:
+    async def __add_dying_emoji_if_required(self, thread) -> str:
         parameters = self.__settings["dying"]
-        archive_date = await self.__get_archive_date(thread)
+        archive_date = await self.__get_thread_archive_date(thread)
         hours_until_archive = (archive_date - self.__date).total_seconds() / 3600
         hours_for_emoji = parameters["hours"]
-        output = f" :{parameters['emoji']}:" if hours_until_archive < hours_for_emoji else ''
+        output = f" :{parameters['emoji']}:" if hours_until_archive < hours_for_emoji else ""
         return output
 
+    async def __update_active_threads_channel(self) -> None:
+        await self.__delete_old_messages()
+        await self.__send_new_messages()
 
-    async def __clean_channel(self) -> None:
-
-        """
-        Delete all messages in Open Threads channel.
-        """
-
+    async def __delete_old_messages(self) -> None:
         async for message in self.__channel_to_update.history(limit=None):
             await message.delete()
-        return
 
-    async def __update_channel(self) -> None:
-
-        """
-        Send messages to Open Threads channel
-        """
-
+    async def __send_new_messages(self) -> None:
         for message in self.__messages:
             await self.__channel_to_update.send(message)
-        return
-
 
 def setup(bot):
     bot.add_cog(ActiveThreads(bot))
